821bd2497feb85680d2fe39440b2f72a61e6a9d9
Nick Wellnhofer <wellnhofer@aevum.de>
2014-03-21 22:05:55 +0100

    Adjust core code for Clownfish changes
    - .cfp file
    - Separate Clownfish parcel
    - IMP renaming
    - Immutable strings
    - Type resolution

--- core/LucyX/Analysis/WhitespaceTokenizer.c
+++ core/LucyX/Analysis/WhitespaceTokenizer.c
@@ -1,13 +1,14 @@
 #define WSTOKER_USE_SHORT_NAMES
+#define CFISH_USE_SHORT_NAMES
 #define LUCY_USE_SHORT_NAMES
 
 #define C_WSTOKER_WHITESPACETOKENIZER
-#include "Lucy/Util/ToolSet.h"
-
 #include "LucyX/Analysis/WhitespaceTokenizer.h"
+
+#include "Clownfish/Util/StringHelper.h"
 #include "Lucy/Analysis/Inversion.h"
 #include "Lucy/Analysis/Token.h"
-#include "Lucy/Util/StringHelper.h"
+#include "Lucy/Util/ToolSet.h"
 
 #include <ctype.h>
 
@@ -29,7 +30,8 @@ WhitespaceTokenizer_init(WhitespaceTokenizer *self) {
 }
 
 Inversion*
-WhitespaceTokenizer_transform(WhitespaceTokenizer *self, Inversion *inversion) {
+WhitespaceTokenizer_Transform_IMP(WhitespaceTokenizer *self,
+                                  Inversion *inversion) {
     Inversion *new_inversion = Inversion_new(NULL);
     Token *token;
 
@@ -42,16 +44,18 @@ WhitespaceTokenizer_transform(WhitespaceTokenizer *self, Inversion *inversion) {
 }
 
 Inversion*
-WhitespaceTokenizer_transform_text(WhitespaceTokenizer *self, CharBuf *text) {
+WhitespaceTokenizer_Transform_Text_IMP(WhitespaceTokenizer *self,
+                                       String *text) {
     Inversion *new_inversion = Inversion_new(NULL);
-    WhitespaceTokenizer_Tokenize_Str(self, (char*)CB_Get_Ptr8(text),
-                                     CB_Get_Size(text), new_inversion);
+    WhitespaceTokenizer_Tokenize_Str(self, (char*)Str_Get_Ptr8(text),
+                                     Str_Get_Size(text), new_inversion);
     return new_inversion;
 }
 
 void
-WhitespaceTokenizer_tokenize_str(WhitespaceTokenizer *self, const char *text,
-                                 size_t len, Inversion *inversion) {
+WhitespaceTokenizer_Tokenize_Str_IMP(WhitespaceTokenizer *self,
+                                     const char *text, size_t len,
+                                     Inversion *inversion) {
     size_t byte_pos       = 0;
     size_t char_pos       = 0;
     size_t start_byte_pos = 0;
@@ -86,8 +90,8 @@ WhitespaceTokenizer_tokenize_str(WhitespaceTokenizer *self, const char *text,
     }
 }
 
-bool_t
-WhitespaceTokenizer_equals(WhitespaceTokenizer *self, Obj *other) {
+bool
+WhitespaceTokenizer_Equals_IMP(WhitespaceTokenizer *self, Obj *other) {
     WhitespaceTokenizer *const twin = (WhitespaceTokenizer*)other;
     if (twin == self)                          { return true; }
     if (!Obj_Is_A(other, WHITESPACETOKENIZER)) { return false; }
